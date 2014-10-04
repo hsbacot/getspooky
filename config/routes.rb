1c0399e4a7f66ba382583cefbca7e51aae460e08
hsbacot <hsbacot@gmail.com>
2014-10-04 13:56:17 -0400

    home layout, header & footer, feed, index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'site#index'
   
   get 'site/index'
+  get 'feed' => 'site#feed'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
